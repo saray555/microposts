ffbacc4e774267b8e3c622539335c122dd6ced09
Noriko Ohnuki <nnorky@gmail.com>
2017-02-02 03:48:51 +0000

    add posting microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,20 +1,12 @@
 class MicropostsController < ApplicationController
-<<<<<<< HEAD
   before_action :logged_in_user, only: [:create, :destroy]
-=======
-  before_action :logged_in_user, only: [:create]
->>>>>>> user-profile
-
+    
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-<<<<<<< HEAD
-=======
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc) # この行を追加
->>>>>>> user-profile
       render 'static_pages/home'
     end
   end
